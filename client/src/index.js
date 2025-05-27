ee1ee3558393b232fdea41663a716d0f6cd1d0fa
Jose Hernandez <josealfredo.066@gmail.com>
2025-05-27 17:22:10 -0400

    add IdeaForm component for submitting ideas and handling modal interactions

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal.js";
 import "./css/style.css";
+import IdeaForm from "./components/IdeaForm.js";
 
 const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
